f22d86b4c0fd51e05f987f1d17be34e226c2caed
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-04-10 14:19:01 +0530

    incrementing patientId based on previous json response count working

--- trials/gatekeeper_sample/index.js
+++ trials/gatekeeper_sample/index.js
@@ -3,10 +3,73 @@ const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
 const Request = require("request");
+const axios = require('axios');
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+// API Call to create new patient
+app.get('/createPatient', async function(req, res) {
+    console.log('API Call to create new patient');
+    console.log(req.query.firstName);
+    console.log(req.query.lastName);
+
+    var jsonResponse;
+    var count;
+
+    axios.get('http://315419af.ngrok.io/api/Patient').then(function (response){
+        console.log(response.data);
+        jsonResponse = response.data;
+        
+    }).then(function (response){
+        findPatientCount();
+    }).catch(function (error) {
+    console.log(error);
+  });
+
+
+  function findPatientCount(){
+
+
+    count =  2005 + jsonResponse.length;
+
+
+
+
+    Request.post({
+        "headers": { "content-type": "application/json" },
+        "url": "http://315419af.ngrok.io/api/Patient",
+        "body": JSON.stringify({
+            "firstName" : req.query.firstName,
+            "lastName" : req.query.lastName,
+            "patientId" : count.toString(),
+            "password" : "adad",
+            "contentKey" : "fsffs"
+        })
+    }, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        console.dir(JSON.parse(body));
+    });
+
+
+  }
+
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    
+    
+    
+    
+    });
+
+
 
 
 app.get('/test', async function(req, res) {
